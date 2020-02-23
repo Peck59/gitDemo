0bf5fe5bdeca5b130b9630ce2af5a5c618230296
Peck <peck.watcharapong@gmail.com>
2020-02-23 17:45:14 +0700

    second

---
+++ ifElse.js
@@ -0,0 +1,10 @@
+if(true){
+    document.write("Multiplication table");
+    document.write("<br>");
+    let result = 1;
+    for(i=1;i<=12;i++){
+        result = 2*i;
+        document.write("2 x "+i+" = "+result)
+        document.write("<br>");
+    }
+}
